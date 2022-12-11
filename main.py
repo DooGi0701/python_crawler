f41d00c5df7363a0badcc2e4ccb45b03172c2786
DooGi0701 <rkddbqls1458@naver.com>
2022-12-11 11:49:44 +0000

    gitpod

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# 여기에 파이썬 코드를 입력하면 됩니다.
+from requests import get
+
+websites = [
+    "google.com",
+    "airbnb.com",
+    "https://twitter.com" "facebook.com",
+    "https://tiktok.com",
+]
+
+results = {}
+
+for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}"
+    response = get(website)
+    if response.status_code == 200:
+        results[website] = "OK"
+    else:
+        results[website] = "FAILED"
+
+print(results)
