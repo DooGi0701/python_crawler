4c927c942e8540f4e76d27b27f5fa5263c105051
DooGi0701 <rkddbqls1458@naver.com>
2022-12-14 13:34:44 +0000

    5.6 job_posts code

--- main.py
+++ main.py
@@ -12,3 +12,5 @@ else:
     jobs = soup.find_all(
         "section", class_="jobs"
     )  # 결과 값을 보고 싶으면 맨 앞에 있는 jobs=를 print()로 바꿔라
+    for job_section in jobs:
+        job_posts = job_section.find_all("li")
