be4a03f85c4f0c54ecd5cd34c3082a6b74a3d3d2
DooGi0701 <rkddbqls1458@naver.com>
2022-12-17 14:49:24 +0000

    5.7 title code -1

--- main.py
+++ main.py
@@ -16,12 +16,21 @@ else:
         job_posts = job_section.find_all("li")
         job_posts.pop(-1)  # 출력항목에 있는 마지막 항목을 제거한다.
         for post in job_posts:
+
             anchors = post.find_all("a")  # html안에 있는 li안에 있는 하이퍼링크 부분을 찾아라
+
             anchor = anchors[1]  # 찾은 하이퍼 링크 부분 중 두번째 자리 부분을 대입하라
+
             link = anchor[
                 "href"
             ]  # href에 접근할 수 있다 Tip : beautiful Soup는 html에 있는 태그들을 파이썬에 있는 dirtionary 처럼 바꿔 줄 수 있다
             # link를 print로 바꾸면 출력물을 볼 수 있음
-            anchor.find_all(
+
+            company, kind, region = anchor.find_all(
                 "span", class_="company"
             )  # html li안에 있는 span부분에서 class가 company가 들어간 모든 것을 찾는다
+            print(
+                company, kind, region
+            )  # class를 보니깐 company라는 클래스가 너무 많아서 파이썬에 있는 문법을 사용해서 출력문을 정리함 ex : company, kind, region = 첫번째, 두번째, 세번째 company가 들어간 class이다.
+
+            title = anchor.find("span", class_="title")
