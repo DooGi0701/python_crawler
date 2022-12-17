1a858e6d70d51219d845cddb3be93b531c757813
DooGi0701 <rkddbqls1458@naver.com>
2022-12-17 14:18:58 +0000

    5.7 Job Extraction anchors code - 1

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ else:
         job_posts = job_section.find_all("li")
         job_posts.pop(-1)  # 출력항목에 있는 마지막 항목을 제거한다.
         for post in job_posts:
-            print(post)
+            anchors = post.find_all("a")  # html안에 있는 li안에 있는 하이퍼링크 부분을 찾아라
+            anchor = anchors[1]  # 찾은 하이퍼 링크 부분 중 두번째 자리 부분을 대입하라
             print(
-                "////////////////////////////////////////////////////////////////"
-            )  # 출력할때 posts끼리 구분을 위함이다
+                anchor["href"]
+            )  # href에 접근할 수 있다 Tip : beautiful Soup는 html에 있는 태그들을 파이썬에 있는 dirtionary 처럼 바꿔 줄 수 있다
